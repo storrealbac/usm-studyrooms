0f267dff9852115faff3025e61e2177815cef764
storrealbac <sebastian.torrealba@usm.cl>
2021-10-11 02:18:27 -0300

    fixed macros bug, changed colors, fixed minors bugs

--- src/views/about.py
+++ src/views/about.py
@@ -4,19 +4,19 @@ def project_license():
     return render_template(
         "about/licencia.html",
         title="Licencia",
-        color_fondo="bg-red-400"
+        color_fondo="bg-color-champagnepink"
     ).replace("&quot;", "\"")
 
 def usage_guide():
     return render_template(
         "about/guia.html",
         title="Guia de uso",
-        color_fondo="bg-red-400"
+        color_fondo="bg-color-champagnepink"
     )
 
 def project_developers():
     return render_template(
         "about/desarrolladores.html",
         title="Desarrolladores",
-        color_fondo="bg-red-400"
+        color_fondo="bg-color-champagnepink"
     )
